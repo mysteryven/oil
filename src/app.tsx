95939f29ebbc780009977b338693864bc01f2219
wenzhe <mysteryven@gmail.com>
2024-10-07 10:26:44 +0800

    fix: acitve index back to parent dir

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Text, useApp, useInput } from 'ink'
-import { useActiveIndex, useCurrentDir, useFileList } from './hooks.js';
+import { Box, Newline, Spacer, Text, useApp, useInput } from 'ink'
+import { useActiveIndex, useCurrentDir, useFileList, usePrevious } from './hooks.js';
 import ActiveText from './activeText.tsx';
+import path from 'node:path';
 
 interface Props {
     initialDir: string
@@ -13,8 +14,14 @@ const App = (props: Props) => {
     const app = useApp()
     const [reloadFlag, setReloadFlag] = useState(0)
     const [currentDir, actions] = useCurrentDir(props.initialDir)
+    const [lastDir, setLastDir] = useState<string>("")
     const list = useFileList(currentDir, [reloadFlag])
-    const initialIndex = Math.min(list.length, 1)
+    let initialIndex = Math.min(list.length, 1)
+
+    // we are doing a parent action
+    if (lastDir) {
+        initialIndex = list.findIndex((fileMeta) => fileMeta.filename === lastDir)
+    }
     const [mode, setMode] = useState<Mode>('Normal')
     const activeIndex = useActiveIndex(initialIndex, list.length, mode, [currentDir, initialIndex])
     const [debug, setDebug] = useState<any>()
@@ -22,7 +29,6 @@ const App = (props: Props) => {
     useEffect(() => {
         setDebug(`reloadFlag${reloadFlag}, currentDir: ${currentDir}, activeIndex: ${activeIndex}, mode: ${mode}`)
     }, [reloadFlag, currentDir, activeIndex, mode])
-    
 
     // switch mode
     useInput((_input, key) => {
@@ -43,44 +49,76 @@ const App = (props: Props) => {
             return
         }
 
+        if (input === 'a' && mode === 'Normal') {
+
+        }
+
         if (key.return) {
             if (list[activeIndex]?.filename === "..") {
+                let parentDir = path.basename(currentDir)
+                setLastDir(parentDir)
+                actions.parent()
                 return
             }
         }
 
         if (input === '-') {
+            let parentDir = path.basename(currentDir)
+            setLastDir(parentDir)
             actions.parent()
         }
         if (input === '=' && list[activeIndex]?.filename) {
             actions.in(list[activeIndex].filename)
+            setLastDir("")
         }
     })
 
     const reload = () => {
         setReloadFlag(prev => prev + 1)
+        setMode('Normal')
     }
 
     return (<>
-        <Box flexDirection="column">
+        <Box flexDirection="column" borderStyle={"round"} flexGrow={1} height={30}>
             {list.map((fileMeta, index) => {
                 const name = fileMeta.type === 'dir' ? fileMeta.filename + '/' : fileMeta.filename
                 if (activeIndex === index) {
-                    return (<ActiveText key={index} mode={mode} fileMeta={fileMeta} reload={reload}>{name}</ActiveText>)
+                    return (<ActiveText key={fileMeta.filename} mode={mode} fileMeta={fileMeta} reload={reload}>{name}</ActiveText>)
                 }
-                return <Box>
+                return <Box key={fileMeta.filename}>
                     <Box flexGrow={1} width={100}>
-                        <Text key={index}>{name}</Text>
+                        <Text>{name}</Text>
                     </Box>
                 </Box>
             })}
-            <Text>{debug}</Text>
+            <Spacer />
+            <Box borderStyle={"double"} flexDirection='column'>
+
+                <Box flexDirection='row'>
+                    <Text>q: Exit </Text>
+                    <Text> | </Text>
+                    <Text>a: Add</Text>
+                    <Text> | </Text>
+                    <Text> &lt;Enter&gt;: Modify </Text>
+                    <Text> | </Text>
+                    <Text>-: Go to parent dir </Text>
+                    <Text> | </Text>
+                    <Text>=: Into dir </Text>
+                </Box>
+                <Box>
+                    <Text color={"gray"}>
+                        <Newline />
+                        {
+                            mode === 'Normal'
+                                ? "Tip: You are in normal mode, you can navigate with arrow keys, press <Enter> to modify the filename"
+                                : "Tip: You are in insert mode, press Esc to undo the modify, press <Enter> to save the modify"
+                        }
+                    </Text>
+                </Box>
+            </Box>
         </Box>
     </>)
 }
-
-
-
 
 export default App
 
--- src/cli.tsx
+++ src/cli.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import App from './app.tsx';
 import { withFullScreen } from './fullScreen/FullScreenBox.tsx';
+import { render } from 'ink';
 
 let currentDir = process.cwd()
 
-withFullScreen(<App initialDir={currentDir} />, {
-    debug: true
-}).start();
+render(<App initialDir={currentDir} />)
 
+// withFullScreen(<App initialDir={currentDir} />)
 
 
 
